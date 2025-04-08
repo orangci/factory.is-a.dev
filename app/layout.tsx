706956681b077bd7eb27fffb7f5009a2504aa808
orangci <orangc@proton.me>
2025-04-08 06:44:34 +0300

    feat: update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'factory.is-a.dev',
+  description: 'The is-a.dev subdomain factory.'
 }
 
 export default function RootLayout({
